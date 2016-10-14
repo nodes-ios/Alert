c59e3609b41d1a3147b6c86dbda38f4144d8bae6
Andrew Lloyd <anll@nodesagency.com>
2016-10-14 17:17:11 +0100

    Demo example project added. Initial Alert VC added with xib and all properties customisable and set when view is shown.

---
+++ AlertDemo/AlertDemo/AlertDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AlertDemo
+//
+//  Created by Andrew Lloyd on 14/10/2016.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showAlertButonTapped(_ sender: AnyObject) {
+        let alert = AlertVC()
+        alert.backgroundColor = UIColor.yellow
+        alert.okButtonBackgroundColor = UIColor.red
+        alert.subLabelText = "subLabel"
+        alert.extraSubLabelText = "more info!"
+        alert.labelText = "Message"
+        alert.forcedAlert = true
+        alert.cornerRadius = 20
+        alert.labelFont = UIFont.boldSystemFont(ofSize: 40)
+        alert.showAlert()
+    }
+
+}
+
